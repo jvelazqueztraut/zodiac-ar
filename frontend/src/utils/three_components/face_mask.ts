781246be0093fd8b80a845512b38c5b762fe9ed4
Javier Velazquez Traut <javier.traut@unit9.com>
2024-10-25 19:28:40 -0300

    (feat) Added Face Mesh occluder and texture test

--- frontend/src/utils/three_components/face_mask.ts
+++ frontend/src/utils/three_components/face_mask.ts
@@ -7,19 +7,28 @@ export default class FaceMask {
   scene: THREE.Scene;
   needsUpdate: boolean;
   landmarks: NormalizedLandmarkList;
-  faces: THREE.Mesh | null;
+  faceOccluder: THREE.Mesh | null;
+  materialOccluder: THREE.Material;
+  faceTexture: THREE.Mesh | null;
+  texture: THREE.Texture;
+  materialTexture: THREE.Material;
   width: number;
   height: number;
-  material: THREE.Material;
 
   constructor(scene: THREE.Scene, width: number, height: number) {
     this.scene = scene;
     this.needsUpdate = false;
     this.landmarks = null;
-    this.faces = null;
+    this.faceOccluder = null;
+    this.materialOccluder = new THREE.MeshNormalMaterial({ colorWrite: false }); // change to true for debugging
+    this.faceTexture = null;
+    this.texture = new THREE.TextureLoader().load('/images/faceMeshPiscis.png');
+    this.materialTexture = new THREE.MeshBasicMaterial({
+      map: this.texture,
+      transparent: true,
+    });
     this.width = width;
     this.height = height;
-    this.material = new THREE.MeshNormalMaterial();
   }
 
   updateDimensions(width: number, height: number) {
@@ -34,26 +43,33 @@ export default class FaceMask {
   }
 
   updateMaterial(material: THREE.Material) {
-    this.material = material;
-    this.material.needsUpdate = true;
+    this.materialTexture = material;
+    this.materialTexture.needsUpdate = true;
   }
 
   addFaces() {
     // create faces
     const geometry = makeGeometry(this.landmarks);
-    this.faces = new THREE.Mesh(geometry, this.material);
-    this.faces.position.set(0, 0, 0);
-    this.faces.scale.set(this.width, this.height, this.width);
-    this.scene.add(this.faces);
+    this.faceOccluder = new THREE.Mesh(geometry, this.materialOccluder);
+    this.faceOccluder.renderOrder = 0; // important to make occluding work
+    this.faceOccluder.position.set(0, 0, 0);
+    this.faceOccluder.scale.set(this.width, this.height, this.width);
+    this.scene.add(this.faceOccluder);
+
+    this.faceTexture = new THREE.Mesh(geometry, this.materialTexture);
+    this.faceTexture.position.set(0, 0, 0);
+    this.faceTexture.scale.set(this.width, this.height, this.width);
+    this.scene.add(this.faceTexture);
   }
 
   removeFaces() {
-    this.scene.remove(this.faces);
+    this.scene.remove(this.faceOccluder);
+    this.scene.remove(this.faceTexture);
   }
 
   update() {
     if (this.needsUpdate) {
-      if (this.faces !== null) {
+      if (this.faceOccluder !== null || this.faceTexture !== null) {
         this.removeFaces();
       }
       if (this.landmarks !== null) {
