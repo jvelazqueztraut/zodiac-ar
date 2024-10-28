f5482b3eb51ee8848464b2228b2477da97b3755c
Javier Velazquez Traut <javier.traut@unit9.com>
2024-10-28 13:31:09 -0300

    (fix) animations

--- frontend/src/utils/styles/animations.ts
+++ frontend/src/utils/styles/animations.ts
@@ -40,7 +40,7 @@ export const landingPageStartButtonMotionProps = {
 export const arPageFaceTrackerMotionProps = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
-  transition: { duration: 2, ease: 'easeInOut' },
+  transition: { duration: 1, ease: 'easeInOut', delay: 0.5 },
 };
 
 export const arPageDraggableSliderMotionProps = {
